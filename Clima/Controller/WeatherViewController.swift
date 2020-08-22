8cc7a19cc27511e1096c2bb8b92f30ed7d88dcaf
LM Bibera <lyndon.bibera@headhuntr.io>
2020-08-22 10:36:24 +0800

    Keyboard stuffs

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,45 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
     }
-
-
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if "" == searchTextField.text {
+            searchTextField.placeholder = "Enter a city..."
+            return false
+        }
+        
+        searchTextField.placeholder = "Search"
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let text = searchTextField.text ?? ""
+        
+        searchTextField.text = ""
+        
+        print(text)
+    }
+    
 }
 
